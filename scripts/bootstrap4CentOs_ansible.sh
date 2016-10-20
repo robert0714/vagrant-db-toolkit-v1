a158034e1830e387168f6906d3e29c0833eb63b8
Robert <robert0714@gmail.com>
2016-10-20 22:13:15 +0800

    Update bootstrap4CentOs_ansible.sh

--- scripts/bootstrap4CentOs_ansible.sh
+++ scripts/bootstrap4CentOs_ansible.sh
@@ -12,7 +12,7 @@ echo "Installing Ansible  on CentOS..."
 
 yum install -y wget
 wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
-rpm -ivh epel-release-7-8.noarch.rpm
+rpm -ivhf  --quiet epel-release-7-8.noarch.rpm
 rm -rf epel-release-7-8.noarch.rpm
 yum -y install ansible
 yum -y install jq
