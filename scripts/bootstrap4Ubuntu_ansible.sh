5643c2e33fb9039a1774a77e3ed239e2dde54e80
Robert <robert0714@gmail.com>
2016-10-27 00:21:57 -0500

    Update bootstrap4Ubuntu_ansible.sh

--- scripts/bootstrap4Ubuntu_ansible.sh
+++ scripts/bootstrap4Ubuntu_ansible.sh
@@ -5,7 +5,7 @@ set -e
 echo "Installing Ansible  on Ubuntu..."
 apt-get install -y software-properties-common
 apt-add-repository ppa:ansible/ansible
-#apt-get update
+apt-get update
 apt-get install -y --force-yes ansible
 cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
 
